44abf5410bd6e536b07939f60283dce817f33b8e
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-02-09 03:25:29 -0600

    stereoCamera kinda works

--- Stereo Depth Map Prototyping (Alex)/cameraTest.py
+++ Stereo Depth Map Prototyping (Alex)/cameraTest.py
@@ -4,13 +4,13 @@
 
 import cv2
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap2 = cv2.VideoCapture(2)
 
 # Check if the webcam is opened correctly
 if not cap.isOpened() or not cap2.isOpened():
     raise IOError("Cannot open webcam")
-stereo = cv2.StereoBM_create(numDisparities=16, blockSize=35)
+stereo = cv2.StereoBM_create(numDisparities=0, blockSize=39)
 
 while True:
     ret, frame = cap.read()
@@ -20,6 +20,8 @@ while True:
     right = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
     disparity = stereo.compute(left, right)
+    disparity = cv2.normalize(disparity, disparity, alpha=100, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
     cv2.imshow('left', left)
     cv2.imshow('right', right)
     cv2.imshow('disparity', disparity)
