91bbf0845004eabf3b1884d2acad00edfe2e61f7
Toningbasher <45208445+Toningbasher@users.noreply.github.com>
2022-03-02 17:33:47 -0600

    Key for Jon and final Script code

---
+++ Movement(Pranay)/finScript.cpp
@@ -0,0 +1,107 @@
+/************************************************************************
+Copyright (c) 2020, Unitree Robotics.Co.Ltd. All rights reserved.
+Use of this source code is governed by the MPL-2.0 license, see LICENSE.
+************************************************************************/
+
+#include "unitree_legged_sdk/unitree_legged_sdk.h"
+#include <math.h>
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+
+using namespace UNITREE_LEGGED_SDK;
+
+class Custom
+{
+	public:
+	Custom(uint8_t level): safe(LeggedType::A1), udp(level){
+	udp.InitCmdData(cmd);
+}
+	void UDPRecv();
+	void UDPSend();
+	void RobotControl();
+
+	Safety safe;
+	UDP udp;
+	HighCmd cmd = {0};
+	HighState state = {0};
+	int motiontime = 0;
+	float dt = 0.002;     // 0.001~0.01
+};
+
+
+void Custom::UDPRecv()
+{
+	udp.Recv();
+}
+
+void Custom::UDPSend()
+{  
+	udp.Send();
+}
+
+void Custom::RobotControl() 
+{
+	cmd.forwardSpeed = 0.0f;
+	cmd.sideSpeed = 0.0f;
+	cmd.rotateSpeed = 0.0f;
+	cmd.bodyHeight = 0.0f;
+
+	cmd.mode = 0;      // 0:idle, default stand      1:forced stand     2:walk continuously
+	cmd.roll  = 0;
+	cmd.pitch = 0;
+	cmd.yaw = 0;
+		
+	while (1) {
+		string command = '';
+		/*
+			GET ETHERNET COMMANDS HERE
+		*/
+		
+		if(command[0] == 'm') {
+			cmd.mode = 2;
+		} else if (command[0] == 's') {
+			cmf.mode = 1;
+		}
+		
+		float angle;
+		if(command[2] == 'l') {
+			angle = stoi(angle.substr(3,5));
+			cmd.yaw = -1 * angle / 180;
+		} else if (command[2] == 'r') {
+			angle = stoi(angle.substr(3,5));
+			cmd.yaw = 1 * angle / 180;
+		}
+		udp.SetSend(cmd);
+	}
+}
+
+int main(void) 
+{
+	//Unitree
+	std::cout << "Communication level is set to HIGH-level." << std::endl
+	      << "WARNING: Make sure the robot is standing on the ground." << std::endl
+	      << "Press Enter to continue..." << std::endl;
+	std::cin.ignore();
+
+	Custom custom(HIGHLEVEL);
+	InitEnvironment();
+	LoopFunc loop_control("control_loop", custom.dt,    boost::bind(&Custom::RobotControl, &custom));
+	LoopFunc loop_udpSend("udp_send",     custom.dt, 3, boost::bind(&Custom::UDPSend,      &custom));
+	LoopFunc loop_udpRecv("udp_recv",     custom.dt, 3, boost::bind(&Custom::UDPRecv,      &custom));
+
+	loop_udpSend.start();
+	loop_udpRecv.start();
+	loop_control.start();
+
+	while(1){
+		sleep(10);
+	};
+
+	return 0; 
+}
